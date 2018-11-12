58888a5d941ce7ab267b7e958825cb41ba109be1
btcarter <benjamin.t.carter@gmail.com>
2018-11-12 14:06:02 -0700

    finishing maskMaker so it can now read 1D files for coordinates and use them to make spherical ROI masks for both centers of mass (CM) and maximal intensity (MI) voxels.

--- maskMaker.sh
+++ maskMaker.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
 #Written by Ben Carter, 11/9/2018
-#This script will create both a functional ROI mask from group statistical results as well as spherical ROI mask using the center of mass to look at participant variability.
+#This script will create both a functional ROI mask and output an RAI table with coordinates for the CM and MI for those ROIs.
 #Dependencies: AFNI
 
 #environmental variables
-PROJECT=Users/ben88/Documents/Research/fMRI_data/Reading    #home directory
+PROJECT=/Users/ben88/Documents/Research/fMRI_data/Reading   #home directory
 SUBJ_DIR=$PROJECT/Compute_data/SubjData                     #directory with individual subject data
 GROUP_DIR=$PROJECT/results/Group_Analysis2/predictability3  #directory with group results
 GROUP_FILE=$GROUP_DIR/pred_lsa+tlrc.HEAD                    #group results file
-TABLE=$GROUP_DIR/functionalTable.txt                        #output table
+WORK_DIR=$GROUP_DIR                                         #working directory or where you want your output files to be created.
+TABLE=$WORK_DIR/roiTable                                    #output table
+MASTER=$GROUP_DIR/cthulhu_mni+tlrc                          #anatomical template must manually update this below
+RADIUS=7.5
+
+#set working directory
+WORK_DIR=$GROUP_DIR
+cd $WORK_DIR
 
 #Commands
 FUNCTIONAL_MASK="3dclust -1Dformat -nosum -1dindex 4 -1tindex 5 -2thresh -3.291 3.291 -dxyz=1 -savemask functionalMask 1.01 38"
-COORDINATES="3dclust -1Dformat -nosum -1dindex 4 -1tindex 5 -2thresh -3.291 3.291 -dxyz=1 1.01 38"
+COORDINATES="3dclust -1Dformat -nosum -orient LPI -nosum -1dindex 4 -1tindex 5 -2thresh -3.291 3.291 -dxyz=1 1.01 38"
 
-#make functional roi mask
+#make functional roi mask and summary table
 $FUNCTIONAL_MASK $GROUP_FILE
 
 #output coordinate data for most active voxels and center of mass
-touch $TABLE
-$COORDINATES $GROUP_FILE >> $TABLE
+touch ${TABLE}.1D
+$COORDINATES $GROUP_FILE >> ${TABLE}.1D
+1dcat ${TABLE}.1D'[1..3]' > ${TABLE}_CM.1D
+1dcat ${TABLE}.1D'[4..6]' > ${TABLE}_MI.1D
 
-#make spherical ROI mask using coordinates file
+#afni makes the spherical mask
+3dUndump -prefix sphereROI -master $MASTER -orient LPI -srad $RADIUS -xyz ${TABLE}_CM.1D
+3dUndump -prefix sphereROIMI -master $MASTER -orient LPI -srad $RADIUS -xyz ${TABLE}_MI.1D
